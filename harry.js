9aee3bded6550ce3f09667b1a29d4d57cf3959e5
Rashmi1526 <rashmi.19b101039@abes.ac.in>
2020-11-07 21:30:28 +0530

    second change

---
+++ harry.js
@@ -0,0 +1,7 @@
+
+function available(){
+    document.getElementById('description').style.display='block';      
+}
+function remove(){
+    document.getElementById('description').style.display='none';      
+}
